bfe1af29ccd3787a6b4505cccce2df4120299c82
TopHaze <xavier.lecocq44@gmail.com>
2020-07-22 15:54:56 +0200

    xavier/22/07/2020/16h00

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\NoticeRepository;
+use App\Entity\Notice;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(NoticeRepository $noticeRepository)
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'notices' => $noticeRepository->findAll(),
         ]);
     }
 }
